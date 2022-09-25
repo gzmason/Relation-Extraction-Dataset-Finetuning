ca8d437dae29ff76c6fc805a53e6df09cd88ecb4
Jessie Wang <yw3765@columbia.edu>
2022-09-25 14:47:41 -0400

    rm print

--- exp_week1/data_parser.py
+++ exp_week1/data_parser.py
@@ -1,6 +1,5 @@
 import pubmed_parser as pp
 SAMPLEDATA_DIR = 'sample_dataset'
 path_xml = pp.list_xml_path(SAMPLEDATA_DIR) # list all xml paths under directory
-print(path_xml)
 pubmed_dict = pp.parse_medline_xml(path_xml[0]) # dictionary output
 print(pubmed_dict[:50])
