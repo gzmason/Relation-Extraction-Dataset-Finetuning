1a48b485aa5659e3e31393c99a40e2894ca7b173
Ruilin Liu <rl3234@columbia.edu>
2022-12-08 21:19:08 -0500

    Delete exp_week1 directory

--- exp_week1/download_dataset.sh
+++
@@ -1,3 +0,0 @@
-mkdir sample_dataset
-wget -P sample_dataset https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed22n0001.xml.gz
-wget -P sample_dataset https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed22n0002.xml.gz
